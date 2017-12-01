c6cf9c3ef303bc944acc551a4ae1ca4b1c14d688
Tom Wadley <tom@tomwadley.net>
2017-11-30 21:34:22 -0600

    A Makey Makey menu interface for switching modes
    And remove cycle script - not needed

---
+++ util/makey-menu.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+
+if (( $# == 0 )); then
+    echo "USAGE: ./makey-menu.sh path/to/vibrate-experiment"
+    echo ""
+    echo "A Makey Makey menu interface for switching modes"
+    exit 1
+fi
+
+BIN=$1
+
+UP="market"
+DOWN="morse"
+#RIGHT=""
+#LEFT=""
+
+pid=""
+
+while :
+do
+    while
+        mode=""
+
+        IFS="" read -n 1 k
+        [ "$k" == "A" ] && mode="$UP"
+        [ "$k" == "B" ] && mode="$DOWN"
+#        [ "$k" == "C" ] && cmd="$RIGHT"
+#        [ "$k" == "D" ] && cmd="$LEFT"
+        [ "$k" == " " ] && exit 0 # Space key exits
+
+        [ "$mode" == "" ]
+    do :; done
+
+    [ "$pid" != "" ] && kill $pid
+
+    cmd="$BIN $mode"
+    echo "Running command '$cmd'"
+
+    $cmd &
+    pid=$!
+    trap "kill $pid" EXIT
+done
