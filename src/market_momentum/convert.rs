de0551b00e8a5e43c703513c9b098e2e56c75e13
Tom Wadley <tom@tomwadley.net>
2017-12-09 22:09:12 -0600

    Invert stock market momentum such that high momentum = low vibration

--- src/market_momentum/convert.rs
+++ src/market_momentum/convert.rs
@@ -6,6 +6,8 @@ const MAX_MOM: f64 = 1.0;
 const MIN_MOM: f64 = -1.0;
 
 pub fn mom_to_vibration(v: f64) -> u8 {
+    let v = -v;
+
     if v > MAX_MOM {
         return MAX_VIB
     }
@@ -20,8 +22,8 @@ pub fn mom_to_vibration(v: f64) -> u8 {
 mod tests {
     #[test]
     fn boundaries() {
-        assert_eq!(super::mom_to_vibration(1.0), 20);
-        assert_eq!(super::mom_to_vibration(-1.0), 0);
+        assert_eq!(super::mom_to_vibration(1.0), 0);
+        assert_eq!(super::mom_to_vibration(-1.0), 20);
     }
 
     #[test]
@@ -31,13 +33,13 @@ mod tests {
 
     #[test]
     fn quarter_values() {
-        assert_eq!(super::mom_to_vibration(0.5), 15);
-        assert_eq!(super::mom_to_vibration(-0.5), 5);
+        assert_eq!(super::mom_to_vibration(0.5), 5);
+        assert_eq!(super::mom_to_vibration(-0.5), 15);
     }
 
     #[test]
     fn out_of_bounds() {
-        assert_eq!(super::mom_to_vibration(1.2), 20);
-        assert_eq!(super::mom_to_vibration(-1.2), 0);
+        assert_eq!(super::mom_to_vibration(1.2), 0);
+        assert_eq!(super::mom_to_vibration(-1.2), 20);
     }
 }
